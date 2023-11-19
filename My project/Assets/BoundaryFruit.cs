acc3bd6cd0581b66c99f56ff78e11a05455f11c8
CasuallyThere <bonynguyentn@gmail.com>
2023-11-19 23:37:20 +1100

    Boundaries and No stacks
    Added Boundaries for player
    Added Lose condition for fruit
    Added Material for Fruit
    Added layer for player and player boundaries
    
    To add:
    Combining
    Score
    Highscore

---
+++ My project/Assets/BoundaryFruit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine;
+
+public class BoundaryFruit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+
+}
--- My project/Assets/Fruit.cs
+++ My project/Assets/Fruit.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Fruit : MonoBehaviour
 {
     private bool dropped = false;
     private Transform fruitPos;
     private Rigidbody2D rigid2d;
+    private bool checktime = false;
+            
+            
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,7 @@ public class Fruit : MonoBehaviour
     void Awake(){
         fruitPos = GetComponent<Transform>();
         rigid2d = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -29,13 +35,42 @@ public class Fruit : MonoBehaviour
 
         if(Input.GetKeyDown("space")){
             rigid2d.gravityScale = 1;
+            
             dropped = true;
-
+            if(Random.value > 0.5f){
+                rigid2d.AddForce(Vector2.left * 0.01f);
+            }
+            else{
+                rigid2d.AddForce(Vector2.left * 0.01f);
+            }
             //Reset controller spawning
             Controller.spawned = false;
+            StartCoroutine(checkEnd());
             
         }
 
+        
+
 
     }
+
+    IEnumerator checkEnd(){
+        yield return new WaitForSeconds(0.75f);
+        checktime = true;
+
+
+    }
+
+    // //Collision
+     void OnTriggerStay2D(Collider2D collision){
+            if((collision.gameObject.name == "BoundaryFruit") && checktime){
+                Debug.Log("GG");
+                SceneManager.LoadScene("SampleScene");
+            }
+
+     }
+
+    
+
+
 }
